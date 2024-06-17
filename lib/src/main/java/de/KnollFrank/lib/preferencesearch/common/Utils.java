19f686f21fdb8768f75d57431d25dd2a58eb2044
Frank Knoll <Knoll_Frank@web.de>
2024-06-17 20:42:38 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/common/Utils.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/common/Utils.java
@@ -1,5 +1,9 @@
 package de.KnollFrank.lib.preferencesearch.common;
 
+import org.threeten.bp.Duration;
+
+import java.util.concurrent.TimeUnit;
+
 public class Utils {
 
     public static <T> Class<? extends T> getClass(final String className) {
@@ -7,6 +11,14 @@ public class Utils {
             return (Class<? extends T>) Class.forName(className);
         } catch (ClassNotFoundException e) {
             throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static void sleep(final Duration duration) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(duration.toMillis());
+        } catch (final InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
